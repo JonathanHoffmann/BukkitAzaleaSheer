aef69bdd38fc344b0b6431d97950cfdf36017c78
Jonathan Hoffmann <jonathan.hoffmann@gmx.net>
2023-04-01 01:49:58 +0200

    Release1.0

--- src/main/java/me/Jonnyfant/BukkitAzaleaSheer/BukkitAzaleaSheer.java
+++
@@ -1,8 +0,0 @@
-package me.Jonnyfant.BukkitAzaleaSheer;
-
-import org.bukkit.plugin.java.JavaPlugin;
-public class BukkitAzaleaSheer extends JavaPlugin { @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
-    }
-}
