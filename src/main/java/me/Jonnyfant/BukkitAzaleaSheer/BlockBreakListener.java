9b25148f7f4729e9858c9b7a40a6c62d3eb88e23
Jonathan Hoffmann <jonathan.hoffmann@gmx.net>
2023-04-15 19:54:11 +0200

    Working dispenser, not working durability from dispenser

--- src/main/java/me/Jonnyfant/BukkitAzaleaSheer/BlockBreakListener.java
+++ src/main/java/me/Jonnyfant/BukkitAzaleaSheer/BlockBreakListener.java
@@ -2,8 +2,6 @@ package me.Jonnyfant.BukkitAzaleaSheer;
 
 import java.util.Random;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
@@ -20,47 +18,26 @@ public class BlockBreakListener implements Listener {
     private final Double CHANCE_UNBREAKING_1 = 0.5;
     private final Double CHANCE_UNBREAKING_2 = 0.66;
     private final Double CHANCE_UNBREAKING_3 = 0.75;
+
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player p = event.getPlayer();
         //https://hub.spigotmc.org/javadocs/spigot/org/bukkit/event/block/BlockBreakEvent.html
         if (p.getInventory().getItemInMainHand().getType().equals(Material.SHEARS)) {
             if (sheerBlock(event.getBlock())) {
-                //handleDurability(p.getInventory().getItemInMainHand());
-                event.setCancelled(true);
+                event.setDropItems(false);
             }
         }
     }
 
-    /*
     @EventHandler
     public void onBlockDispense(BlockDispenseEvent event) {
-        Bukkit.broadcastMessage("Block: " + event.getBlock().getType() + " Stack " + event.getItem().toString());
         if (event.getItem().getType().equals(Material.SHEARS)) {
-            //weird stuff that needs to be done to get the Block the Dispenser is facing
             Dispenser d = (Dispenser) event.getBlock().getState().getData();
             Block target = event.getBlock().getRelative(d.getFacing());
-            Bukkit.broadcastMessage("Harvesting " + target.getType());
-            target.setType(Material.AIR);
-            Bukkit.broadcastMessage("Position: " + target.getLocation().toString());
-
             if (sheerBlock(target)) {
-                handleDurability(event.getItem());
-                event.setCancelled(true);
+                target.setType(Material.AIR);
             }
-        }
-    }
-    */
-
-    //test version
-    @EventHandler
-    public void onBlockDispense(BlockDispenseEvent event) {
-        Bukkit.broadcastMessage("Block: " + event.getBlock().getType() + " Stack " + event.getItem().toString());
-        if (event.getItem().getType().equals(Material.SHEARS)) {
-            //weird stuff that needs to be done to get the Block the Dispenser is facing
-            Dispenser d = (Dispenser) event.getBlock().getState().getData();
-            Block target = event.getBlock().getRelative(d.getFacing());
-            target.breakNaturally(event.getItem());
         }
     }
 
@@ -81,24 +58,23 @@ public class BlockBreakListener implements Listener {
         Damageable meta = (Damageable) is.getItemMeta();
         Random random = new Random();
         if (is.containsEnchantment(Enchantment.DURABILITY)) {
-            switch(is.getEnchantmentLevel(Enchantment.DURABILITY)) {
+            switch (is.getEnchantmentLevel(Enchantment.DURABILITY)) {
                 case 1:
-                    if(random.nextDouble()>CHANCE_UNBREAKING_1)
+                    if (random.nextDouble() > CHANCE_UNBREAKING_1)
                         meta.setDamage(meta.getDamage() + 1);
                     break;
                 case 2:
-                    if(random.nextDouble()>CHANCE_UNBREAKING_2)
+                    if (random.nextDouble() > CHANCE_UNBREAKING_2)
                         meta.setDamage(meta.getDamage() + 1);
                     break;
                 case 3:
-                    if(random.nextDouble()>CHANCE_UNBREAKING_3)
+                    if (random.nextDouble() > CHANCE_UNBREAKING_3)
                         meta.setDamage(meta.getDamage() + 1);
                     break;
             }
         } else {
-            meta.setDamage(meta.getDamage() - 1);
+            meta.setDamage(meta.getDamage() + 1);
         }
-
         is.setItemMeta(meta);
         return is;
     }
